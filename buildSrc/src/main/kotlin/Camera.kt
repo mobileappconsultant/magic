05f953a19ea7f7a546949577631cee4830f33d59
Samson Akinsanya <info@mobeparadigm.co.uk>
2022-10-07 00:10:51 +0100

    Initial commit

---
+++ buildSrc/src/main/kotlin/Camera.kt
@@ -0,0 +1,7 @@
+object Camera {
+    private const val cameraxStableVersion = "1.0.1"
+    private const val cameraxAlphaVersion = "1.0.0-alpha28"
+    const val camera = "androidx.camera:camera-camera2:$cameraxStableVersion"
+    const val cameraLifecycle =  "androidx.camera:camera-lifecycle:$cameraxStableVersion"
+    const val cameraView = "androidx.camera:camera-view:$cameraxAlphaVersion"
+}
